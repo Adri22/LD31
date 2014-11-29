a702a4139f956f911a48fb9ac29deb14ccd65205
Adrian Böhm <adrian.boehm22@googlemail.com>
2014-11-29 22:06:27 +0100

    init libgdx

---
+++ html/src/de/_1nulleins0/ld31/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package de._1nulleins0.ld31.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de._1nulleins0.ld31.Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Game();
+        }
+}
