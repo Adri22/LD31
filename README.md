c1c8c89130b6dbdb7e78bce39b0fc2294e838944
Adrian Böhm <adrian.boehm22@googlemail.com>
2014-11-29 22:20:49 +0100

    readme and window-resolution

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ LD31
 My Ludum Dare 31 project
 
 .. coming soon.
+
+
+used tools:
+- eclipse
+- libGDX
+- Paint.net
+- ...
--- desktop/src/de/_1nulleins0/ld31/desktop/DesktopLauncher.java
+++ desktop/src/de/_1nulleins0/ld31/desktop/DesktopLauncher.java
@@ -5,8 +5,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de._1nulleins0.ld31.Game;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
-	}
+    public static void main(String[] arg) {
+	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+	config.title = "LD31";
+	config.width = Game.resolution.width;
+	config.height = Game.resolution.height;
+	config.resizable = false;
+
+	new LwjglApplication(new Game(), config);
+    }
 }
